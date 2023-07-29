0eb5016790c4ca4fe9a7f7c27cd14e3435a4b617
nonaka-motoya <nonmot0614@chiba-u.jp>
2023-07-29 13:33:55 +0900

    :bug: add parameter file

--- momentum/calc_mom.sh
+++ momentum/calc_mom.sh
@@ -1,2 +1,2 @@
 mkdir ./output
-./calc_momentum -V ./input_files/vtx_info_nuall_00010-00039_p500_numucc_v20230706.txt -I ./input_files/LTList.txt.debug -O ./output/vtx_info.txt
+./calc_momentum -V ./input_files/vtx_info_nuall_00010-00039_p500_numucc_v20230706.txt -I ./input_files/LTList.txt.debug -O ./output/vtx_info.txt -P ../par/MC_plate_1_100.txt
